59f8ea58c0091e3fc45cd0f5c2ba0e7226e856d5
atrislab <atrislab@gmail.com>
2023-06-30 11:10:40 +0200

    puntos y coma fuera

--- src/math.js
+++ src/math.js
@@ -1,5 +1,5 @@
-const sum = (a, b) => a + b;
-const mul = (a, b) => a * b;
+const sum = (a, b) => a + b
+const mul = (a, b) => a * b
 const sub = (a, b) => a - b
 const div = (a, b) => a / b;
 const calculatePercentage = (num, percentage) => (num*percentage)/100
